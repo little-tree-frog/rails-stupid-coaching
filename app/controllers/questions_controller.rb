e6c0a59f42c0029da4dde7fdcb1fd87eedc802f2
Sonya Duke <sonya.j.duke@gmail.com>
2020-04-21 21:19:40 +1000

    Stupid coaching app functionality and CSS done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @answer = params[:answer]
+    @print_me = ""
+    if @answer.include?("?")
+      @print_me = "Silly question, get dressed and go to work!"
+    elsif @answer == "i am going to work" || @answer == "I am going to work"
+      @print_me = "Great!"
+    else
+      @print_me = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
